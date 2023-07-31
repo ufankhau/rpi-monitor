1535d1ad0b0f94b564bf6b89540c575358b56617
Urs Fankhauser <ufankhau@gmail.com>
2023-07-31 10:54:09 +0200

    ..

--- rpi-update.sh
+++ rpi-update.sh
@@ -2,7 +2,7 @@
 
 # settings
 name=$(hostname)
-os=$(/usr/bin/cat /etc/os-release | /usr/bin/egrep -i 'pretty_name' | /usr/bin/awk -F'\"' '{print $2}')
+osName=$(/usr/bin/cat /etc/os-release | /usr/bin/egrep -i 'pretty_name' | /usr/bin/awk -F'\"' '{print $2}')
 logDIR=/home/pi/log
 logfn=$logDIR/${name}-update-os.log
 if [[ ! -d $logDIR ]]; then mkdir $logDIR; chown pi:pi $logDIR; fi 
@@ -14,18 +14,18 @@ timestamp()
 }
 
 # main body
-echo "$(timestamp)" >> $logfin
-echo "$(timestamp) *** upgrade ${os} on ${^name}" >> $logfn
+echo "$(timestamp)" >> $logfn
+echo "$(timestamp) *** upgrade ${osName} on ${name^}" >> $logfn
 echo "$(timestamp) ***" >> $logfn
 /usr/bin/apt-get upgrade -y 2>$1 >> $logfn
 if [[ $? = 0 ]]; then
-    echo "$(timestamp) ***" >> $logfin
-    echo "$(timestamp) *** upgrade successfully completed!" >> $logfin
-    echo "$(timestamp)" >> $logfin
+    echo "$(timestamp) ***" >> $logfn
+    echo "$(timestamp) *** upgrade successfully completed!" >> $logfn
+    echo "$(timestamp)" >> $logfn
 else
-    echo "$(timestamp) ***" >> $logfin
-    echo "$(timestamp) *** ERROR occured during upgrade, please check!" >> $logfin
-    echo "$(timestamp)" >> $logfin
+    echo "$(timestamp) ***" >> $logfn
+    echo "$(timestamp) *** ERROR occured during upgrade, please check!" >> $logfn
+    echo "$(timestamp)" >> $logfn
     exit 5
 fi
 
