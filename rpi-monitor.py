097162c9bee368965984d8e720b5ef07f2d3a783
Urs Fankhauser <ufankhau@gmail.com>
2023-08-09 10:28:01 +0200

    ..

--- rpi-monitor.py
+++ rpi-monitor.py
@@ -804,7 +804,7 @@ for [sensor, params] in detectorValues.items():
         payload["stat_t"] = values_topic_rel
         payload[
             "val_tpl"
-        ] = "{{{{ value_json.{}.{} | replace(' %', '') | replace(' °C', '') }}}}".format(
+        ] = "{{{{ value_json.{}.{} | replace('%', '') | replace('°C', '') }}}}".format(
             LDS_PAYLOAD_NAME, params["json_value"]
         )
 
@@ -943,8 +943,8 @@ def sendStatus(timestamp, nothing):
     rpiData["Memory_Installed"] = "{} {}".format(
         rpi_memory_installed, rpi_memory_installed_unit
     )
-    rpiData["Memory_Used"] = rpi_memory_used
-    rpiData["Temperature_CPU"] = "{} °C".format(rpi_cpu_temp)
+    rpiData["Memory_Used"] = "{}%".format(rpi_memory_used)
+    rpiData["Temperature_CPU"] = "{}°C".format(rpi_cpu_temp)
     rpiData["Temperature_GPU"] = rpi_gpu_temp
     rpiData[RPI_CPU_LOAD_1M] = rpi_cpu_load_1m
     rpiData[RPI_CPU_LOAD_5M] = rpi_cpu_load_5m
